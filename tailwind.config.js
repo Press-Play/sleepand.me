c343cf3d7a2f4bf89fd0cc100006d075e28df442
Khanh Nguyen <dagger_906@hotmail.com>
2024-10-02 23:36:22 +1000

    First

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'teal-main': '#29E8B2',
+      }
+    },
+  },
+  plugins: [],
+}
+
